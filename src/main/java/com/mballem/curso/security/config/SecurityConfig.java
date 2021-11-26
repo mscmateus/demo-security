8a228bc126d49614de47aaa68684048d29b8eff3
Mateus Costa <msc.mateus@hotmail.com>
2021-11-26 12:48:11 -0500

    feat - configurações de login

--- src/main/java/com/mballem/curso/security/config/SecurityConfig.java
+++ src/main/java/com/mballem/curso/security/config/SecurityConfig.java
@@ -1,11 +1,19 @@
 package com.mballem.curso.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.mballem.curso.security.service.UsuarioService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private UsuarioService service;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -16,12 +24,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		.and()
 		.formLogin()
 		.loginPage("/login")
-		.defaultSuccessUrl("/",true)
+		.defaultSuccessUrl("/",true) 
 		.failureUrl("/login-error")
 		.permitAll()
 		.and()
 		.logout()
 		.logoutSuccessUrl("/");
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(service).passwordEncoder(new BCryptPasswordEncoder());
 	}
 	
 }
