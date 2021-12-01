6c20ef0a275a5e4e977a407207c455822009059a
Mateus Costa <msc.mateus@hotmail.com>
2021-12-01 11:06:35 -0500

    feat - configuração de acesso e tratamento de excessões para paciente

--- src/main/java/com/mballem/curso/security/repository/UsuarioRepository.java
+++ src/main/java/com/mballem/curso/security/repository/UsuarioRepository.java
@@ -1,6 +1,8 @@
 package com.mballem.curso.security.repository;
 
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -21,5 +23,5 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	@Query("select distinct u from Usuario u"
 			+" inner join u.perfis p"
 			+" where u.id = :usuarioId AND p.id IN :perfisId")
-	Usuario findByIdAndPerfis(Long usuarioId, Long[] perfisId);
+	Optional<Usuario> findByIdAndPerfis(Long usuarioId, Long[] perfisId);
 }
